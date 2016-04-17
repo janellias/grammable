a8eab9c1038332ade281602ef2cf7c533f8a4cf8
Janel Stedtler <jstedtler@gmail.com>
2016-04-17 19:27:15 +0000

    Added FactoryGirl

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -15,11 +15,7 @@ RSpec.describe GramsController, type: :controller do
     end
 
     it "should successfully show the new form" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-        )
+      user = FactoryGirl.create(:user)
       sign_in user
 
       get :new
@@ -36,11 +32,7 @@ RSpec.describe GramsController, type: :controller do
 
 
     it "should successfully create a new gram in our database" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryGirl.create(:user)
       sign_in user
 
       post :create, gram: {message: 'Hello!'}
@@ -53,13 +45,10 @@ RSpec.describe GramsController, type: :controller do
     end 
 
     it "should properly deal with validation errors" do
-        user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryGirl.create(:user)
       sign_in user
 
+      gram_count = Gram.count
       post :create, gram: {message: '' }
       expect(response).to have_http_status(:unprocessable_entity)
       expect(Gram.count).to eq 0
